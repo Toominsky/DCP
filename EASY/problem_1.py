f62914341942de648a642f43a1f7d04ea8d85bf1
Tomasz Wadas <tomasz.wadas@hotmail.com>
2023-02-04 18:01:02 +0100

    added oneliner to problem1.

--- EASY/problem_1.py
+++ EASY/problem_1.py
@@ -6,15 +6,19 @@ Bonus: Can you do this in one pass?'''
 
 
 def check_sum(array, k):
-
     for first in array:
         for second in array:
             if first + second == k:
                 return print(f'{first} + {second} = {k}')
 
 
-array = [10, 15, 3, 7]
+def checK_sum_oneliner(array, k):
+    return tuple(x for x in array for y in array if x + y == k)
+
+array = [10, 15, 3, 7, 2, 6]
 k = 17
+k2 = 8
 
 if __name__ == '__main__':
     check_sum(array, k)
+    print(checK_sum_oneliner(array,k2))
